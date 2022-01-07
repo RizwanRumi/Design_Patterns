855393e8bfabf7e54aba71fb8811fe67cda0153e
RizwanRumi <rizwan.cse33@gmail.com>
2022-01-07 18:07:05 +0100

    ...

---
+++ Python/Creational_Design_Pattern/Factory.py
@@ -0,0 +1,89 @@
+from abc import ABCMeta, abstractstaticmethod
+
+"""
+Factory pattern provides an interfaces for creating object in a superclass, 
+but allows subclasses to alter the type of object that will be created.
+
+using a Factory allows the consumer to create new objects without having 
+to know the details of how they're created, or what their dependencies 
+are - they only have to give the information they actually want.
+
+"""
+
+# create interface
+class ITransport(metaclass=ABCMeta):
+
+    @staticmethod
+    def get_deliver():
+        """The product interface"""
+
+class Truck():
+
+    def __init__(self):
+        self.package = "Standard"
+        self.max_product_weight = 50
+        self.max_delivery_days = 7
+        self.cost = 15
+
+    def get_deliver(self):
+        return{"Package": self.package, "max. carry": self.max_product_weight,
+               "max. delivery day": self.max_delivery_days, "cost": self.cost }
+
+class Ship():
+
+    def __init__(self):
+        self.package = "Standard"
+        self.max_product_weight = 100
+        self.max_delivery_days = 15
+        self.cost = 10
+
+    def get_deliver(self):
+        return{"Package": self.package, "max. carry": self.max_product_weight,
+               "max. delivery day": self.max_delivery_days, "cost": self.cost }
+
+
+class Car():
+
+    def __init__(self):
+        self.package = "Standard"
+        self.max_product_weight = 20
+        self.max_delivery_days = 4
+        self.cost = 12
+
+    def get_deliver(self):
+        return {"Package": self.package, "max. carry": self.max_product_weight,
+                "max. delivery day": self.max_delivery_days, "cost": self.cost}
+
+class TransportAgency():
+
+    @staticmethod
+    def get_transport(transport_type):
+        try:
+            if transport_type == "Truck":
+                print("Truck:")
+                return Truck()
+            elif transport_type == "Ship":
+                print("Ship:")
+                return Ship()
+            elif transport_type == "Car":
+                print("Car:")
+                return Car()
+            else:
+                raise AssertionError("Transport is not found")
+
+        except AssertionError as error:
+            print(error)
+
+
+if __name__ == "__main__":
+    Transport = TransportAgency.get_transport("Truck")
+    print("\t {}".format(Transport.get_deliver()))
+
+    Transport = TransportAgency.get_transport("Car")
+    print("\t {}".format(Transport.get_deliver()))
+
+    Transport = TransportAgency.get_transport("Ship")
+    print("\t {}".format(Transport.get_deliver()))
+
+    Transport = TransportAgency.get_transport("Air")
+    print("\t {}".format(Transport))
