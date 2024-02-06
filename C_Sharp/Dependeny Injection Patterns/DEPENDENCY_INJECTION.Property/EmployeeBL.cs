af8ffc956ea38f01afde2f2c847fe6d51081662a
EKVIP\rrah <r.rahman@ekvip.de>
2024-02-06 11:06:27 +0100

    Add Dependency Injection using Property example

---
+++ C_Sharp/Dependeny Injection Patterns/DEPENDENCY_INJECTION.Property/EmployeeBL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEPENDENCY_INJECTION.Property
+{
+    //This is the Class that is going to consume the services provided by the IEmployeeDAL Class
+    //That means it is the Dependent Class which Depending on the IEmployeeDAL Class
+    public class EmployeeBL
+    {
+        public IEmployeeDAL employeeDAL;
+
+        // Injecting the Dependency Object using public property
+        public IEmployeeDAL EmployeeDataObject
+        {
+            set
+            {
+                this.employeeDAL = value;
+            }
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return employeeDAL.SelectAllEmployees();
+        }
+    }
+}
---
+++ C_Sharp/Dependeny Injection Patterns/DEPENDENCY_INJECTION.Property/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEPENDENCY_INJECTION.Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dependency Injection Design Pattern Example using Property:");
+
+            // Create instance of client class
+            EmployeeBL employeeBL = new EmployeeBL();
+
+            // Inject the dependency object using the public property of the client class
+            employeeBL.EmployeeDataObject = new EmployeeDAL();
+
+            List<Employee> ListEmployee = employeeBL.GetAllEmployees();
+            foreach (Employee emp in ListEmployee)
+            {
+                Console.WriteLine($"ID = {emp.ID}, Name = {emp.Name}, Department = {emp.Department}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
